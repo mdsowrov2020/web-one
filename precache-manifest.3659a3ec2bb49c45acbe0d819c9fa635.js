7a86da2b2444a8776b0b8dba4abcfca22679ece3
Antu <prince.anto97@gmai.com>
2020-07-03 22:16:29 +0600

    Updates

---
+++ precache-manifest.3659a3ec2bb49c45acbe0d819c9fa635.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2fdb93f62641a50d00c9789f8d454e",
+    "url": "/web-one/index.html"
+  },
+  {
+    "revision": "8e8fd2a03426bef8df2a",
+    "url": "/web-one/static/css/main.e3591560.chunk.css"
+  },
+  {
+    "revision": "97eb3e0453a3e96b0a4c",
+    "url": "/web-one/static/js/2.e28bd1b8.chunk.js"
+  },
+  {
+    "revision": "a480b247b7b7d7b7c447723d55eb8193",
+    "url": "/web-one/static/js/2.e28bd1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8fd2a03426bef8df2a",
+    "url": "/web-one/static/js/main.f5106614.chunk.js"
+  },
+  {
+    "revision": "f2db6189cdae559e5682",
+    "url": "/web-one/static/js/runtime-main.1dadc89b.js"
+  },
+  {
+    "revision": "81ebc153a733ec67e625845af6c19e28",
+    "url": "/web-one/static/media/ArmanSir.81ebc153.jpg"
+  },
+  {
+    "revision": "c93e770da206ad007853d6f3917278e2",
+    "url": "/web-one/static/media/contact.c93e770d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-one/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3777552662e3614d0d0efa421a48654b",
+    "url": "/web-one/static/media/model-1.37775526.jpeg"
+  },
+  {
+    "revision": "40e85e6c7775c5d8bace0f840be629dd",
+    "url": "/web-one/static/media/money.40e85e6c.png"
+  },
+  {
+    "revision": "5e3978ea62ebe61c904f2db25c477235",
+    "url": "/web-one/static/media/project.5e3978ea.png"
+  },
+  {
+    "revision": "5477ec348e86d7dfa4307d59ac8deb50",
+    "url": "/web-one/static/media/seo.5477ec34.png"
+  },
+  {
+    "revision": "f70a52d46992171730568e9105ef30fb",
+    "url": "/web-one/static/media/service.f70a52d4.png"
+  }
+]);
